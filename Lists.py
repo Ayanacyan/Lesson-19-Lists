ad700441904c8e48d8fcdf3c26d6a93899a0bb3d
Ayanacyan <amitpacl@gmail.com>
2026-03-01 21:01:23 -0600

    Lists

---
+++ Lists.py
@@ -0,0 +1,12 @@
+empty_list=[]
+print()
+
+numbers=[1, 2, 3, 4, 5, 6, 7]
+print(numbers)
+
+triple=[6,7]*3
+print(triple)
+
+aList=[100, 150, 200, 250, 300, 350, 400, 450, 500]
+aList=aList[::-1]
+print(aList, '\n')
